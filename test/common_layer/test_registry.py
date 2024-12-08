46c9e4964d1af19879dccb257dee38b6b23ca1ae
sangwon lee <leeh8911@gmail.com>
2024-12-09 06:48:35 +0900

    Test. Update test.
    - test_all_registry_need_to_be_independently 에서 테스트 방식을 멤버 변수 사용하지 않고, 주어진 인터페이스로만으로 변경

--- test/common_layer/test_registry.py
+++ test/common_layer/test_registry.py
@@ -1,10 +1,11 @@
 import pytest
 
 
-from deep_studio.common_layer.registry import REGISTRY_FACTORY
+from deep_studio.common_layer.registry import REGISTRY_FACTORY, RegistryError
 
 
-def test_basic_regist_check():
+def test_basic_register_check():
+    """Register의 기본 기능에 대해 확인하는 테스트"""
 
     TEST_REGISTRY = REGISTRY_FACTORY("TEST_REGISTRY")
 
@@ -31,6 +32,7 @@ def test_basic_regist_check():
 
 
 def test_all_registry_need_to_be_independently():
+    """두 개 이상의 Register가 있을 때, REGISTRY는 독립적인지를 테스트"""
 
     FIRST_REGISTRY = REGISTRY_FACTORY("FIRST_REGISTRY")
     SECOND_REGISTRY = REGISTRY_FACTORY("SECOND_REGISTRY")
@@ -47,5 +49,11 @@ def test_all_registry_need_to_be_independently():
         def do_something(self):
             self.logger.info("this is Foo class")
 
-    assert not "Foo" in SECOND_REGISTRY.REGISTRY
-    assert "Foo" in FIRST_REGISTRY.REGISTRY
+    assert FIRST_REGISTRY.build(
+        **{"name": "Foo", "spiderman": "Tom", "ironman": "Robert"}
+    )
+
+    with pytest.raises(RegistryError):
+        SECOND_REGISTRY.build(
+            **{"name": "Foo", "spiderman": "Tom", "ironman": "Robert"}
+        )
