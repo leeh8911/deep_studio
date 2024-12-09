6f245feefab7716dee3f693c8b0520d11901552c
sangwon lee <leeh8911@gmail.com>
2024-12-10 05:57:52 +0900

    Feat. add registries

---
+++ deep_studio/data_layer/data_registry.py
@@ -0,0 +1,6 @@
+"""data_registry.py
+"""
+
+from deep_studio.common_layer.registry import make_registry
+
+DATA_REGISTRY = make_registry("DATA_REGISTRY")
---
+++ deep_studio/model_layer/model_registry.py
@@ -0,0 +1,8 @@
+"""model_registry.py
+"""
+
+from deep_studio.common_layer.registry import make_registry
+
+MODEL_REGISTRY = make_registry("MODEL_REGISTRY")
+MODEL_INTERFACE_REGISTRY = make_registry("MODEL_INTERFACE_REGISTRY")
+LOSS_REGISTRY = make_registry("LOSS_REGISTRY")
